94c29a6351043891515bbf9ba47e00f3baa17686
i.katsantas <i.katsantas@xe.gr>
2024-05-29 22:09:34 +0300

    Query 1 with csv

---
+++ query_1/query_1_df_csv.py
@@ -0,0 +1,34 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.window import Window
+
+DATA_PARQUET_PATH = "hdfs://master:9000/parquet/Crime_Data"
+
+#############################
+# Preparation
+#############################
+spark = SparkSession.builder \
+    .appName("Query 1 - DataFrame API") \
+    .getOrCreate()
+
+df = spark.read.csv(DATA_PARQUET_PATH, header=True, inferSchema=True)
+
+
+##############################
+# Querying
+##############################
+window_spec = Window.partitionBy("year").orderBy(col("crime_total").desc())
+
+(df
+ .withColumn("Year", substring("DATE OCC", 7, 4))
+ .withColumn("Month", substring("DATE OCC", 1, 2))
+ .groupBy("Year", "Month")
+ .count()
+ .orderBy("Year", "Month")
+ .withColumnRenamed("count", "crime_total")
+ .withColumn("rank", rank().over(window_spec))
+ .filter("rank <= 3")
+ .show(1000))
+
+spark.stop()
+
---
+++ query_1/query_1_sql_csv.py
@@ -0,0 +1,39 @@
+from pyspark.sql import SparkSession
+
+DATA_PARQUET_PATH = "hdfs://master:9000/parquet/Crime_Data"
+
+#############################
+# Preparation
+#############################
+spark = SparkSession.builder \
+    .appName("Query 1 - SQL API") \
+    .getOrCreate()
+
+df = spark.read.csv(DATA_PARQUET_PATH, header=True, inferSchema=True)
+
+
+#############################
+# Querying
+#############################
+df.createOrReplaceTempView("crime_data")
+
+query = """
+
+WITH rankings AS (
+    SELECT year, month, crime_total, RANK() OVER (PARTITION BY year ORDER BY crime_total DESC) as ranking
+    FROM (
+        SELECT SUBSTRING(`DATE OCC`, 7, 4) as year, SUBSTRING(`DATE OCC`, 1, 2) as month, COUNT(*) as crime_total
+        FROM crime_data
+        GROUP BY year, month
+    )
+)
+SELECT * FROM rankings WHERE ranking <= 3 ORDER BY year ASC, ranking ASC
+
+"""
+
+result = spark.sql(query)
+result.show(1000)
+
+
+spark.stop()
+
