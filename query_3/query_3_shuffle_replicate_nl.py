b438c78783e1cd87e3106f69c07f9c312d222e9f
i.katsantas <i.katsantas@xe.gr>
2024-06-08 15:04:52 +0300

    Still trying to run replicate..

--- query_3/query_3_shuffle_replicate_nl.py
+++ query_3/query_3_shuffle_replicate_nl.py
@@ -38,7 +38,7 @@ conf = SparkConf() \
 
 spark = SparkSession.builder \
     .appName("Query 3 - SQL API - Shuffle Replicate NL") \
-    .conf(conf=conf) \
+    .config(conf=conf) \
     .getOrCreate()
 
 df_crimes = spark.read.csv(CRIME_DATA_CSV_PATH, header=True, inferSchema=True)
