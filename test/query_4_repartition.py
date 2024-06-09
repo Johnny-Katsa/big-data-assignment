5611907b1136ebb52bce1190d6cdab5939921bed
i.katsantas <i.katsantas@xe.gr>
2024-06-10 02:43:51 +0300

    Manual repartition testing..

--- test/query_4_repartition.py
+++ test/query_4_repartition.py
@@ -23,7 +23,7 @@ police_stations_rdd = spark.read.csv(STATION_LOCATIONS_CSV_PATH, header=True, in
 # Repartitioning
 #####################################################################
 # Converting datasets to dictionary key-value pairs which can be repartitioned
-crimes_key_values = crime_data_rdd.map(lambda x: (x['AREA'], (x['DR_NO'], 'crime')))
+crimes_key_values = crime_data_rdd.map(lambda x: (x['AREA'], (x, 'crime')))
 police_stations_key_values = police_stations_rdd.map(lambda x: (x['PREC'], (x['OBJECTID'], 'station')))
 
 united = crimes_key_values.union(police_stations_key_values)
