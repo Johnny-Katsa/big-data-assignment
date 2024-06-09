3506bbdcc18b28c00d41563fdeb723050a9cbbf8
i.katsantas <i.katsantas@xe.gr>
2024-06-10 01:18:23 +0300

    Manual repartition testing..

--- test/query_4_repartition.py
+++ test/query_4_repartition.py
@@ -28,7 +28,7 @@ police_stations_key_values = police_stations_rdd.map(lambda x: (x['PREC'], (x, '
 
 
 def my_reduce(key, records):
-    return [key, records]
+    return [key]
 
 
 # united = united.reduceByKey(my_reduce)
